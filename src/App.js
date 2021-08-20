52566fa7d5d1f60405ba0dc02439b2a748498d20
Shri12345patil <jayshri.patil@sts.in>
2021-08-20 21:33:33 +0530

    signup code commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./index.css";
+
+export default function App() {
+  return (
+    <div class="form-container">
+      <h3>SignUp</h3>
+      <form class="register-form">
+        <input
+          id="first-name"
+          class="form-field"
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+        />
+        <input
+          id="last-name"
+          class="form-field"
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+        />
+        <input
+          id="email"
+          class="form-field"
+          type="text"
+          placeholder="Email"
+          name="email"
+        />
+        <button class="form-field" type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+}
